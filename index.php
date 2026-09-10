49ab7424087b0b7da7fc6f1634d0d2f0e982bd33
kari <iwanfadill37@gmail.com>
2026-09-10 17:56:16 +0800

    feat:adding login.php and modifying index.php

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php 
-
-require_once 'config/database.php';
+session_start();
 require_once 'includes/function.php';
+
+if(!isset($_SESSION['login'])) {
+    header("Location: login.php");
+}
+
+$url = isset($_GET['url']) ? rtrim(isset($_GET['url']), '/') : 'dashboard';
+
+$url_segments = explode('/', $url);
+$modul = $url_segments[0];
+
+// url list herry
+switch($modul){
+    case 'login':
+        include 'modules/auth/login.php';
+        break;
+
+    default:
+        echo "404 url not find king herry is angry";
+        break;
+}
 
 echo "<h1>Selamat Datang di Web Kasir!</h1>";
 echo "<p>koneksi ke Database PDO berhasil di jalankan</p>";
